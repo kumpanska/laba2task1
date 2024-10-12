f35ef1c6478c14e82e44821f08a2ee27fab547ee
Kate Kumpanska <kate.kumpansky@gmail.com>
2024-10-12 11:03:42 +0300

    add partial class and field

---
+++ labaa2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labaa2
+{
+    public partial class MyMatrix
+    {
+        double[,] array;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
