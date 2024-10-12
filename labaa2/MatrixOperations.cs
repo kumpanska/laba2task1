3fe891864ed023910f8294c7db5fafbb1a5e0300
Kate Kumpanska <kate.kumpansky@gmail.com>
2024-10-12 20:27:14 +0300

    add operator +

--- labaa2/MatrixOperations.cs
+++ labaa2/MatrixOperations.cs
@@ -1,14 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace labaa2
 {
     public partial class MyMatrix
-    { 
-     
+    {
+        public static MyMatrix operator +(MyMatrix matrix1, MyMatrix matrix2)
+        {
+            if (matrix1.Height != matrix2.Height || matrix1.Width != matrix2.Width)
+            {
+                throw new ArgumentException("Matrixes should be the same size");
+            }
+            MyMatrix newMatrix = new MyMatrix(new double[matrix1.Height, matrix1.Width]);
+            for (int i = 0; i < matrix1.Height; i++)
+            {
+                for (int j = 0; j < matrix1.Width; j++)
+                {
+                    newMatrix.array[i,j] = matrix1.array[i, j] + matrix2.array[i, j];
+                }
+            }
+            return newMatrix;
+        }
     }
     internal class MatrixOperations
     {
