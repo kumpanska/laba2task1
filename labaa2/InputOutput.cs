b1c4006ce929ea27b4560d2eab11fdae077d343c
Kate Kumpanska <kate.kumpansky@gmail.com>
2024-10-12 23:36:36 +0300

    add input method

--- labaa2/InputOutput.cs
+++ labaa2/InputOutput.cs
@@ -8,5 +8,19 @@ namespace labaa2
 {
     internal class InputOutput
     {
+        public void Input(MyMatrix matrix)
+        {
+            double[,] array = new double[matrix.Height, matrix.Width];
+            for (int i = 0; i < matrix.Height; i++)
+            {
+                string[] elements = Console.ReadLine().Split(' ');
+                for (int j = 0; j < matrix.Width; j++)
+                {
+                    array[i, j] = Convert.ToDouble(elements[j]);
+                }
+            }
+
+        }
+      
     }
 }
